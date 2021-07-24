ddd35640d0a3b52a965de325ad35a8aced7ff078
muammarahlnn <21muammarahlan@gmail.com>
2021-07-25 00:56:47 +0800

    update Posko3Activity

--- app/src/main/java/com/example/myapplication/Posko3Activity.java
+++ app/src/main/java/com/example/myapplication/Posko3Activity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Posko3Activity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class Posko3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posko_3);
+
+        ImageView btnBack = findViewById(R.id.btn_back_posko3);
+        btnBack.setOnClickListener(v -> finish());
     }
 }
